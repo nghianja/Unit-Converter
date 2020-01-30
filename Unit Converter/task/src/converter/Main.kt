ab3f056dfd9064d517f72ec990535dc958478434
James Ng <james.ng@projapps.com>
2020-01-31 04:06:50 +0800

    Complete Stage #2

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,9 +1,10 @@
 package converter
 
+import java.util.Scanner
+
 fun main() {
-    println("145 centimeters is 1.45 meters")
-    println("2 miles is 3.2187 kilometers")
-    println("5.5 inches is 139.7 millimeters")
-    println("12 degrees Celsius is 53.6 degrees Fahrenheit")
-    println("3 pounds is 1.360776 kilograms")
+    val scanner = Scanner(System.`in`)
+    print("Enter a number of kilometers: ")
+    val kilometers = scanner.nextInt()
+    println("$kilometers kilometers is ${kilometers * 1000} meters")
 }
